e8acd24585ada63ddcdab0d0f1085cb01c98c783
myounghaklee <devhak.enzo@gmail.com>
2025-02-19 11:50:20 +0900

    create first, secont service project

---
+++ first-service/src/main/java/com/example/first_service/FirstServiceController.java
@@ -0,0 +1,16 @@
+package com.example.first_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service")
+public class FirstServiceController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to the First service.";
+    }
+
+}

43e38e5264e5d0f88e949cbfa97f1800308a275a
devhak <devhak.enzo@gmail.com>
2025-03-03 21:36:15 +0900

    first, second test api create

--- first-service/src/main/java/com/example/first_service/FirstServiceController.java
+++ first-service/src/main/java/com/example/first_service/FirstServiceController.java
@@ -1,16 +1,26 @@
 package com.example.first_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/")
     public String welcome(){
         return "Welcome to the First service.";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "hello word in first service";
+    }
+
 
 }
